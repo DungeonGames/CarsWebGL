a3513e2a81819ab4974d0b67ce44dec92986320e
Shennod <96147981+Shennod@users.noreply.github.com>
2022-09-20 22:01:49 +0300

    Add keyboard control

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -7,6 +7,7 @@ public class PlayerInput : MonoBehaviour
 
     private Vector2 _direction = new Vector2();
     private bool _enableInput = true;
+    private bool _isKeyboardInput = true;
 
     public event UnityAction<Vector2> Driving;
     public event UnityAction Stopped;
@@ -15,18 +16,32 @@ public class PlayerInput : MonoBehaviour
     {
         if (_enableInput)
         {
-            _direction.Set(_joystick.Horizontal, _joystick.Vertical);
-            Driving?.Invoke(_direction);
+            if (_isKeyboardInput)
+            {
+                float horizontalInput = Input.GetAxis("Horizontal");
+                float verticalInput = Input.GetAxis("Vertical");
 
-            if (_direction == Vector2.zero)
-                Stopped?.Invoke();
+                _direction.Set(horizontalInput, verticalInput);
+                Driving?.Invoke(_direction);
+
+                if (_direction == Vector2.zero)
+                    Stopped?.Invoke();
+            }
+            else
+            {
+                _direction.Set(_joystick.Horizontal, _joystick.Vertical);
+                Driving?.Invoke(_direction);
+
+                if (_direction == Vector2.zero)
+                    Stopped?.Invoke();
+            }
         }
         else
         {
             _direction = Vector2.zero;
             Driving?.Invoke(_direction);
         }
-        
+
     }
 
     public void StopMove() => _enableInput = false;
